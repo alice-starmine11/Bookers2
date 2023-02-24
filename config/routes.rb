72ca47e6d5ce30db05966a45a07b3e2b39e24531
alice-starmine11 <121926699+alice-starmine11@users.noreply.github.com>
2023-02-24 15:40:36 +0000

    [Add] gem,devise,model,controller,Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'homes/about'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'homes/top'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
