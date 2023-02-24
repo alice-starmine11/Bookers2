9b74e0c1dba273900cb5f96d554284db29f2e809
alice-starmine11 <121926699+alice-starmine11@users.noreply.github.com>
2023-02-24 17:59:10 +0000

    [Add] view2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,14 +28,18 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title,:body,:image)
+    params.require(:book).permit(:title,:body)
   end
 end
